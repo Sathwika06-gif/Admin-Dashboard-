0974bc2ea83d23675fce71548f2c2b49a7f9fbb3
Sathwika Kota <kotasathwika02@gmail.com>
2026-05-12 21:47:11 +0530

    Add files via upload

---
+++ Front-End/src/app/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  login(data: any) {
+    return this.http.post<any>('/api/auth/login', data);
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+
+  isLoggedIn() {
+    return !!this.getToken();
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+}
